64fd08b5bea5ce23ec04f51d70373e7d40e14ce8
Janmayjay-singh <janmayjaysingh16@gmail.com>
2021-01-15 21:36:06 +0530

    Create intray top bar and button #2

---
+++ App/todoapp/lib/models/global.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+Color darkGreyColor = new Color(0xFF212128);
+Color redColor = new Color(0xFFE33E57);
+
+TextStyle intrayTitleStyle = new TextStyle(
+  fontFamily: 'Avenir', 
+  fontWeight: FontWeight.bold, 
+  color: darkGreyColor, 
+  fontSize: 50);
+
